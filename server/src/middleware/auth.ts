4eadf413a4c47d803101b6d18582f1b620046383
Davey Schulz <daveschulz@hotmail.nl>
2023-03-17 19:26:31 +0100

    Added middleware to check if the user has already a token

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { StatusCodes } from 'http-status-codes';
+import { Response, Request, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    let token = req.header('Authorization');
+
+    if (!token) return res.status(StatusCodes.FORBIDDEN).send('Access Denied');
+
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length).trimStart();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    req.user = verified;
+
+    next();
+  } catch (err) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: err.message });
+  }
+};
--- server/src/modules/auth/auth.router.ts
+++ server/src/modules/auth/auth.router.ts
@@ -1,8 +1,9 @@
+import { verifyToken } from './../../middleware/auth';
 import express from 'express';
 import { loginHandler } from './auth.controller';
 
 const router = express.Router();
 
-router.post('/login', loginHandler);
+router.post('/login', verifyToken, loginHandler);
 
 export default router;
